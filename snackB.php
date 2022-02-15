47333745de28ee11f33f1cb370d9404098923a0e
Your Name <yan.vi193@gmail.com>
2022-02-15 16:46:01 +0100

    snack-b

--- snackB.php
+++ snackB.php
@@ -5,3 +5,19 @@
     un punto e una chiocciola e che age sia un numero. 
     Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 -->
+
+<?php
+    $nome = $_GET['name'];
+    $email = $_GET['mail'];
+    $eta = $_GET['age'];
+
+    if (empty($nome) === false && empty($email) === false && empty($eta) === false) {
+        if(strlen($nome) > 3 && strpos($email, '@') !== false && strpos($email, '.') !== false && is_numeric($eta)){
+            echo 'Accesso riuscito';
+        }else{
+            echo 'Accesso negato';
+        }
+    }else{
+        echo "Verifica di avere inserito tutti i dati richiesti";
+    }
+?>
--- snackC.php
+++ snackC.php
@@ -2,3 +2,7 @@
     Creare un array con 15 numeri casuali, tenendo conto che l’array 
     non dovrà contenere lo stesso numero più di una volta
 -->
+
+<?php
+
+?>
